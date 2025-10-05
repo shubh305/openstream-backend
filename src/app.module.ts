9ab5e799f469051c38aa0a1ebbf82c3339597839
Shubham Singh <shubham.305singh@gmail.com>
2025-10-05 12:00:00 +0530

    feat: add user module and core configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,40 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MediaServerModule } from './media/media-server.module';
+import { GatewayModule } from './gateway/gateway.module';
+import { AuthModule } from './auth/auth.module';
+
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'vod'),
+      serveRoot: '/vods',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    MediaServerModule,
+    GatewayModule,
+    AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
---
+++ src/users/users.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UsersRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(userData: Partial<User>): Promise<UserDocument> {
+    const newUser = new this.userModel(userData);
+    return newUser.save();
+  }
+
+  async findOne(filter: Record<string, any>): Promise<UserDocument | null> {
+    return this.userModel.findOne(filter).exec();
+  }
+
+  async findByUsername(username: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async findByStreamKey(streamKey: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ streamKey }).exec();
+  }
+
+  async update(user: UserDocument): Promise<UserDocument> {
+    return user.save();
+  }
+}
